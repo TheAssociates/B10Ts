1c20321e6e6bbce0ee4a6a18e00a6f75f3348012
NamesAreAPain <jbcrocker@pacbell.net>
2016-11-02 16:42:09 -0700

    Test

--- Test.java
+++ Test.java
@@ -3,20 +3,17 @@ import java.util.*;
 public class Test{
 	
 	public static void main(String[]args){
+		
 		NonSensor aa = new NonSensor(false);
 		NonSensor ab = new NonSensor(true);
 		Gate a = new Gate(new Readable[]{aa,ab},new int[][]{new int[]{0},new int[]{0}},pRule.OR.rule);
+		Gate ac = new Gate(new Readable[]{a}, new int[][]{new int[]{0}}, pRule.NOT.rule);
 		Memory c = new Memory(1);
-		Connection b = new Connection(a, new int[]{0}, c, new int[]{0});
+		Connection b = new Connection(ac, new int[]{0}, c, new int[]{0});
 		NoMouthMustScream d = new NoMouthMustScream(c,new int[]{0},"hello world");
 		
-		System.out.println(Arrays.toString((pRule.OR.rule.a(new boolean[]{false,true}))));
-
-		
-		
-		Chip e = new Chip(new Component[]{aa,ab,a,b,c,d});
+		Chip e = new Chip(new Component[]{aa,ab,a,ac,b,c,d});
 		e.tick();
-		
 		
 	}
 }
