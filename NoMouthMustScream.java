ed12ebd4a01698dd0ff6f7149ade6bab97c5b2ec
NamesAreAPain <jbcrocker@pacbell.net>
2016-11-02 00:18:52 -0700

    IT STAYS THIS TIME

--- NoMouthMustScream.java
+++ NoMouthMustScream.java
@@ -1,1 +1,14 @@
-public class 
+public class NoMouthMustScream extends Muscle{
+	
+	private String message;
+	
+	public NoMouthMustScream(Readable readFromMem,int[] readFromPos, String message){
+		this.readFromMem = readFromMem;
+		this.readFromPos = readFromPos;
+		this.message = message;
+	}
+	
+	public void update(){
+		System.out.println(message);
+	}
+}
